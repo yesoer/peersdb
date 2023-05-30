875123467d36a29957949f7c738dff961db872ae
yesoer <luisenglaender@web.de>
2023-05-30 23:49:50 +0200

    feat(config): persistent, writable config

---
+++ config.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type Config struct {
+	StoreAddr string `json:"storeAddr"`
+}
+
+// TODO : store config and cache in appropriate directories
+//
+// loads the persistent config file into config struct
+func LoadConfig() (*Config, error) {
+	filename := *flagRepo + "_config"
+
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// default config in case none was found
+			config := &Config{
+				StoreAddr: "transactions",
+			}
+			return config, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+// writes persistent config file from config struct
+func SaveConfig(config *Config) error {
+	filename := *flagRepo + "_config"
+
+	data, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
