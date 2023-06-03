8ca0f9b4bd80bada6e545baef02f26d4cd7c0b44
yesoer <luisenglaender@web.de>
2023-06-03 18:42:27 +0200

    feat(http): use the http service

--- config/flags.go
+++ config/flags.go
@@ -3,6 +3,8 @@ package config
 import "flag"
 
 var FlagShell = flag.Bool("shell", false, "enable shell interface")
+var FlagHTTP = flag.Bool("http", false, "enable http interface")
+
 var FlagExp = flag.Bool("experimental", true, "enable experimental features")
 var FlagPort = flag.String("port", "4001", "configure application port")
 var FlagRepo = flag.String("repo", "peersdb", "configure the repo/directory name for the ipfs node")
