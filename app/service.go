496a422ecefbda53b659d2f4c0c24b33c795c922
yesoer <luisenglaender@web.de>
2023-06-03 18:06:34 +0200

    refactor(service): move to subpackage

--- app/service.go
+++ app/service.go
@@ -1,10 +1,9 @@
-package main
+package app
 
 import (
 	"errors"
-	"peersdb/api"
-	"peersdb/app"
 	"peersdb/config"
+	"peersdb/ipfs"
 	"time"
 
 	orbitdb "berty.tech/go-orbit-db"
@@ -15,10 +14,31 @@ import (
 	"golang.org/x/net/context"
 )
 
-func service(peersDB *app.PeersDB,
-	reqChan chan api.Request,
+// Method defines simple schemas for actions to perform on the db
+type Method struct {
+	Cmd    string
+	ArgCnt int
+}
+
+var (
+	GET     Method = Method{"get", 1}     // needs the filepath
+	POST    Method = Method{"post", 1}    // needs the cid
+	CONNECT Method = Method{"connect", 1} // needs the peer iden
+	QUERY   Method = Method{"query", 0}
+)
+
+// Requests are an abstraction for the communication between this applications
+// various apis (shell, http, grpc etc.) and the actual db service
+// (n to 1 relation at the moment)
+type Request struct {
+	Method Method
+	Args   []string
+}
+
+func Service(peersDB *PeersDB,
+	reqChan chan Request,
 	resChan chan interface{},
-	logChan chan app.Log) {
+	logChan chan Log) {
 
 	coreAPI := (*peersDB.Orbit).IPFS()
 
@@ -33,7 +53,7 @@ func service(peersDB *app.PeersDB,
 	subipfs, err := (*peersDB.Node).PeerHost.EventBus().Subscribe(
 		new(event.EvtPeerConnectednessChanged))
 	if err != nil {
-		logChan <- app.Log{Type: app.NonRecoverableErr, Data: err}
+		logChan <- Log{Type: NonRecoverableErr, Data: err}
 		return
 	}
 
@@ -52,7 +72,7 @@ func service(peersDB *app.PeersDB,
 				cidDbId := (*db).Address().String()
 				err := coreAPI.PubSub().Publish(ctx, peerId, []byte(cidDbId))
 				if err != nil {
-					logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+					logChan <- Log{Type: RecoverableErr, Data: err}
 				}
 
 				continue
@@ -68,7 +88,7 @@ func service(peersDB *app.PeersDB,
 	nodeId := peersDB.ID
 	sub, err := coreAPI.PubSub().Subscribe(ctx, nodeId)
 	if err != nil {
-		logChan <- app.Log{Type: app.NonRecoverableErr, Data: err}
+		logChan <- Log{Type: NonRecoverableErr, Data: err}
 		return
 	}
 
@@ -79,7 +99,7 @@ func service(peersDB *app.PeersDB,
 			// received data should contain the id of the peers db
 			msg, err := sub.Next(context.Background())
 			if err != nil {
-				logChan <- app.Log{Type: app.NonRecoverableErr, Data: err}
+				logChan <- Log{Type: NonRecoverableErr, Data: err}
 				return
 			}
 
@@ -106,7 +126,7 @@ func service(peersDB *app.PeersDB,
 
 				store, err := (*peersDB.Orbit).Open(ctx, addr, &dbopts)
 				if err != nil {
-					logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+					logChan <- Log{Type: RecoverableErr, Data: err}
 				}
 
 				db := store.(iface.EventLogStore)
@@ -128,22 +148,22 @@ func service(peersDB *app.PeersDB,
 		var res interface{}
 
 		switch req.Method.Cmd {
-		case api.GET.Cmd:
+		case GET.Cmd:
 			res = "Not implemented"
-		case api.POST.Cmd:
+		case POST.Cmd:
 			db := peersDB.EventLogDB
 			if db == nil {
 				err = errors.New("you need a datastore first, try connecting to a peer")
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 				break
 			}
 
 			// add a file to the ipfs store
 			// DEVNOTE : test file : "./sample-data/r4.xlarge_i-0f4e4b248a6aa957a_wordcount_spark_large_3/sar.csv"
 			path := req.Args[0]
-			filePath, err := AddToIPFSByPath(ctx, coreAPI, path)
+			filePath, err := ipfs.AddToIPFSByPath(ctx, coreAPI, path)
 			if err != nil {
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 				break
 			}
 
@@ -151,25 +171,25 @@ func service(peersDB *app.PeersDB,
 			data := []byte(filePath.String())
 			_, err = (*db).Add(ctx, data)
 			if err != nil {
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 			}
 
 			res = "File uploaded"
 
-		case api.CONNECT.Cmd:
+		case CONNECT.Cmd:
 			peerId := req.Args[0]
-			err = ConnectToPeers(ctx, peersDB, []string{peerId}, logChan)
+			err = ipfs.ConnectToPeers(ctx, peersDB.Orbit, []string{peerId})
 			if err != nil {
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 			}
 			res = "Peer id processed"
 
 		// TODO : only works once ?!
-		case api.QUERY.Cmd:
+		case QUERY.Cmd:
 			db := peersDB.EventLogDB
 			if db == nil {
 				err = errors.New("you need a datastore first, try connecting to a peer")
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 				break
 			}
 
@@ -179,7 +199,7 @@ func service(peersDB *app.PeersDB,
 			time.Sleep(time.Second * 5)
 			res, err = (*db).List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
 			if err != nil {
-				logChan <- app.Log{Type: app.RecoverableErr, Data: err}
+				logChan <- Log{Type: RecoverableErr, Data: err}
 			}
 		}
 
