ccc44e0cd167d6ffdf2cc18ed2d6e24f45bc852e
yesoer <luisenglaender@web.de>
2023-06-04 16:21:03 +0200

    feat(http): add command endpoint
    Also some refactoring. Response headers and options are handled by a middleware and we use a flag for the port to listen on

--- config/flags.go
+++ config/flags.go
@@ -5,8 +5,10 @@ import "flag"
 var FlagShell = flag.Bool("shell", false, "enable shell interface")
 var FlagHTTP = flag.Bool("http", false, "enable http interface")
 
-var FlagExp = flag.Bool("experimental", true, "enable experimental features")
-var FlagPort = flag.String("port", "4001", "configure application port")
+var FlagIPFSPort = flag.String("ipfs-port", "4001", "configure ipfs port")
+var FlagHTTPPort = flag.String("http-port", "8080", "configure http port")
+
+var FlagExp = flag.Bool("experimental", true, "enable ipfs experimental features")
 var FlagRepo = flag.String("repo", "peersdb", "configure the repo/directory name for the ipfs node")
-var FlagDevLogs = flag.Bool("devlogs", false, "enable development level logging")
-var FlagRoot = flag.Bool("root", false, "creating a root node means creating a new datastore")
+var FlagDevLogs = flag.Bool("devlogs", false, "enable development level logging for orbitdb")
+var FlagRoot = flag.Bool("root", false, "creating a root node means it's possible to create a new datastore")
