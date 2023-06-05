f2f1e0353b9c9806d4126882f742e35c62039119
yesoer <luisenglaender@web.de>
2023-06-05 17:01:59 +0200

    refactor(service): move code to separate functions

--- app/service.go
+++ app/service.go
@@ -9,6 +9,7 @@ import (
 	orbitdb "berty.tech/go-orbit-db"
 	"berty.tech/go-orbit-db/accesscontroller"
 	"berty.tech/go-orbit-db/iface"
+	"berty.tech/go-orbit-db/stores/operation"
 	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/network"
 	"golang.org/x/net/context"
@@ -40,109 +41,18 @@ func Service(peersDB *PeersDB,
 	resChan chan interface{},
 	logChan chan Log) {
 
-	coreAPI := (*peersDB.Orbit).IPFS()
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel() // TODO : correct ?
-
-	//-------------------------------------------------------------------------
-	// EVENTS
-	//-------------------------------------------------------------------------
-
-	// subscribe to ipfs level connectedness changed event
-	subipfs, err := (*peersDB.Node).PeerHost.EventBus().Subscribe(
-		new(event.EvtPeerConnectednessChanged))
-	if err != nil {
-		logChan <- Log{Type: NonRecoverableErr, Data: err}
-		return
-	}
-
 	// wait and handle connectedness changed event
-	go func() {
-		db := peersDB.EventLogDB
-
-		for e := range subipfs.Out() {
-			e, ok := e.(event.EvtPeerConnectednessChanged)
-
-			// on established connection
-			if ok && e.Connectedness == network.Connected && db != nil {
-				// send this stores id to peer by publishing it to the topic
-				// identified by their id
-				peerId := e.Peer.String()
-				cidDbId := (*db).Address().String()
-				err := coreAPI.PubSub().Publish(ctx, peerId, []byte(cidDbId))
-				if err != nil {
-					logChan <- Log{Type: RecoverableErr, Data: err}
-				}
-
-				continue
-			}
-		}
-	}()
-
-	//-------------------------------------------------------------------------
-	// PubSub
-	//-------------------------------------------------------------------------
-
-	// subscribe to own topic
-	nodeId := peersDB.ID
-	sub, err := coreAPI.PubSub().Subscribe(ctx, nodeId)
-	if err != nil {
-		logChan <- Log{Type: NonRecoverableErr, Data: err}
-		return
-	}
+	go awaitConnected(peersDB, logChan)
 
 	// wait for pubsub messages to self which will be received when another peer
-	// connects (see "EVENTS" section : "EvtPeerConnectednessChanged")
-	go func() {
-		for {
-			// received data should contain the id of the peers db
-			msg, err := sub.Next(context.Background())
-			if err != nil {
-				logChan <- Log{Type: NonRecoverableErr, Data: err}
-				return
-			}
-
-			// in case we started without any db, replicate this one
-			if peersDB.EventLogDB == nil {
-				addr := string(msg.Data())
-				create := false
-				storeType := "eventlog"
-
-				// give anyone write access
-				ac := &accesscontroller.CreateAccessControllerOptions{
-					Access: map[string][]string{
-						"write": {
-							"*",
-						},
-					},
-				}
-
-				dbopts := orbitdb.CreateDBOptions{
-					AccessController: ac,
-					Create:           &create,
-					StoreType:        &storeType,
-				}
-
-				store, err := (*peersDB.Orbit).Open(ctx, addr, &dbopts)
-				if err != nil {
-					logChan <- Log{Type: RecoverableErr, Data: err}
-				}
-
-				db := store.(iface.EventLogStore)
-				db.Load(ctx, -1)
-				peersDB.EventLogDB = &db
-
-				// persist store address
-				peersDB.Config.StoreAddr = addr
-				config.SaveConfig(peersDB.Config)
-			}
-		}
-	}()
+	// connects (see "awaitConnected" above)
+	go awaitStoreExchange(peersDB, logChan)
 
 	//--------------------------------------------------------------------------
 	// handle API requests
-	// TODO : send results back to api via respond channel
+
+	// TODO : for http api it would be nice to return errors instead of
+	// logging them
 	for {
 		req := <-reqChan
 		var res interface{}
@@ -151,59 +61,172 @@ func Service(peersDB *PeersDB,
 		case GET.Cmd:
 			res = "Not implemented"
 		case POST.Cmd:
-			db := peersDB.EventLogDB
-			if db == nil {
-				err = errors.New("you need a datastore first, try connecting to a peer")
-				logChan <- Log{Type: RecoverableErr, Data: err}
-				break
-			}
-
-			// add a file to the ipfs store
-			// DEVNOTE : test file : "./sample-data/r4.xlarge_i-0f4e4b248a6aa957a_wordcount_spark_large_3/sar.csv"
 			path := req.Args[0]
-			filePath, err := ipfs.AddToIPFSByPath(ctx, coreAPI, path)
-			if err != nil {
-				logChan <- Log{Type: RecoverableErr, Data: err}
-				break
-			}
-
-			// add the reference to the file (stored in ipfs) to orbitdb
-			data := []byte(filePath.String())
-			_, err = (*db).Add(ctx, data)
-			if err != nil {
-				logChan <- Log{Type: RecoverableErr, Data: err}
-			}
-
-			res = "File uploaded"
+			res = post(peersDB, path, logChan)
 
 		case CONNECT.Cmd:
 			peerId := req.Args[0]
-			err = ipfs.ConnectToPeers(ctx, peersDB.Orbit, []string{peerId})
-			if err != nil {
-				logChan <- Log{Type: RecoverableErr, Data: err}
-			}
-			res = "Peer id processed"
+			res = connect(peersDB, peerId, logChan)
 
-		// TODO : only works once ?!
 		case QUERY.Cmd:
-			db := peersDB.EventLogDB
-			if db == nil {
-				err = errors.New("you need a datastore first, try connecting to a peer")
-				logChan <- Log{Type: RecoverableErr, Data: err}
-				break
-			}
-
-			infinity := -1
-			(*db).Load(ctx, infinity)
-			// TODO : await replication/ready event
-			time.Sleep(time.Second * 5)
-			res, err = (*db).List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
-			if err != nil {
-				logChan <- Log{Type: RecoverableErr, Data: err}
-			}
+			res = query(peersDB, logChan)
 		}
 
 		// send response
 		resChan <- res
 	}
+}
+
+// waits for connectedness changed events and on success sends the stores id
+func awaitConnected(peersDB *PeersDB, logChan chan Log) {
+	// subscribe to ipfs level connectedness changed event
+	subipfs, err := (*peersDB.Node).PeerHost.EventBus().Subscribe(
+		new(event.EvtPeerConnectednessChanged))
+	if err != nil {
+		logChan <- Log{Type: NonRecoverableErr, Data: err}
+		return
+	}
+
+	db := peersDB.EventLogDB
+	coreAPI := (*peersDB.Orbit).IPFS()
+
+	for e := range subipfs.Out() {
+		e, ok := e.(event.EvtPeerConnectednessChanged)
+
+		// on established connection
+		if ok && e.Connectedness == network.Connected && db != nil {
+			// send this stores id to peer by publishing it to the topic
+			// identified by their id
+			peerId := e.Peer.String()
+			cidDbId := (*db).Address().String()
+			ctx := context.Background()
+			err := coreAPI.PubSub().Publish(ctx, peerId, []byte(cidDbId))
+			if err != nil {
+				logChan <- Log{Type: RecoverableErr, Data: err}
+			}
+
+			continue
+		}
+	}
+}
+
+// on connectedness changed events, peers exchange their event logs
+func awaitStoreExchange(peersDB *PeersDB, logChan chan Log) {
+	// subscribe to own topic
+	nodeId := peersDB.ID
+	coreAPI := (*peersDB.Orbit).IPFS()
+	ctx := context.Background()
+	sub, err := coreAPI.PubSub().Subscribe(ctx, nodeId)
+	if err != nil {
+		logChan <- Log{Type: NonRecoverableErr, Data: err}
+		return
+	}
+
+	for {
+		// received data should contain the id of the peers db
+		msg, err := sub.Next(context.Background())
+		if err != nil {
+			logChan <- Log{Type: NonRecoverableErr, Data: err}
+			return
+		}
+
+		// in case we started without any db, replicate this one
+		if peersDB.EventLogDB == nil {
+			addr := string(msg.Data())
+			create := false
+			storeType := "eventlog"
+
+			// give anyone write access
+			ac := &accesscontroller.CreateAccessControllerOptions{
+				Access: map[string][]string{
+					"write": {
+						"*",
+					},
+				},
+			}
+
+			dbopts := orbitdb.CreateDBOptions{
+				AccessController: ac,
+				Create:           &create,
+				StoreType:        &storeType,
+			}
+
+			store, err := (*peersDB.Orbit).Open(ctx, addr, &dbopts)
+			if err != nil {
+				logChan <- Log{Type: RecoverableErr, Data: err}
+			}
+
+			db := store.(iface.EventLogStore)
+			db.Load(ctx, -1)
+			peersDB.EventLogDB = &db
+
+			// persist store address
+			peersDB.Config.StoreAddr = addr
+			config.SaveConfig(peersDB.Config)
+		}
+	}
+}
+
+// executes post command
+func post(peersDB *PeersDB, path string, logChan chan Log) interface{} {
+	db := peersDB.EventLogDB
+	if db == nil {
+		err := errors.New("you need a datastore first, try connecting to a peer")
+		logChan <- Log{Type: RecoverableErr, Data: err}
+		return err
+	}
+
+	// add a file to the ipfs store
+	// DEVNOTE : test file : "./sample-data/r4.xlarge_i-0f4e4b248a6aa957a_wordcount_spark_large_3/sar.csv"
+	ctx := context.Background()
+	coreAPI := (*peersDB.Orbit).IPFS()
+	filePath, err := ipfs.AddToIPFSByPath(ctx, coreAPI, path)
+	if err != nil {
+		logChan <- Log{Type: RecoverableErr, Data: err}
+		return err
+	}
+
+	// add the reference to the file (stored in ipfs) to orbitdb
+	data := []byte(filePath.String())
+	_, err = (*db).Add(ctx, data)
+	if err != nil {
+		logChan <- Log{Type: RecoverableErr, Data: err}
+		return err
+	}
+
+	return "File uploaded"
+}
+
+// executes connect command
+func connect(peersDB *PeersDB, peerId string, logChan chan Log) string {
+	ctx := context.Background()
+	err := ipfs.ConnectToPeers(ctx, peersDB.Orbit, []string{peerId})
+	if err != nil {
+		logChan <- Log{Type: RecoverableErr, Data: err}
+	}
+	return "Peer id processed"
+}
+
+// executes query command
+func query(peersDB *PeersDB, logChan chan Log) []operation.Operation {
+	db := peersDB.EventLogDB
+	if db == nil {
+		err := errors.New("you need a datastore first, try connecting to a peer")
+		logChan <- Log{Type: RecoverableErr, Data: err}
+		return nil
+	}
+
+	infinity := -1
+	ctx := context.Background()
+	(*db).Load(ctx, infinity)
+
+	// TODO : await replication/ready event
+	time.Sleep(time.Second * 5)
+
+	res, err := (*db).List(ctx, &orbitdb.StreamOptions{Amount: &infinity})
+	if err != nil {
+		logChan <- Log{Type: RecoverableErr, Data: err}
+	}
+
+	return res
 }
