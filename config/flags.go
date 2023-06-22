aae25f25656fc23cf1afe0cbbcd200a2f2db43bc
yesoer <luisenglaender@web.de>
2023-06-22 19:40:27 +0200

    feat(service): implement get

--- config/flags.go
+++ config/flags.go
@@ -12,3 +12,4 @@ var FlagExp = flag.Bool("experimental", true, "enable ipfs experimental features
 var FlagRepo = flag.String("repo", "peersdb", "configure the repo/directory name for the ipfs node")
 var FlagDevLogs = flag.Bool("devlogs", false, "enable development level logging for orbitdb")
 var FlagRoot = flag.Bool("root", false, "creating a root node means it's possible to create a new datastore")
+var FlagDownloadDir = flag.String("download-dir", "~/Downloads/", "the destination path for downloaded data")
