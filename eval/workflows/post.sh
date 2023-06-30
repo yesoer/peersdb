a94b36d9711b6dbab91b2be34fbbfc8b9bcfc46b
yesoer <luisenglaender@web.de>
2023-06-30 03:59:32 +0200

    feat(evaluation): post workflow with a file

--- eval/workflows/post.sh
+++ eval/workflows/post.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-# this file is supposed to be used by script_k8s.yaml
+# This file may be used by script_k8s.yaml to run a simple post action on the 
+# network, starting with the root node and posting a file from C3O.
+
+# TODO : git clone to post a file from C3P
 
 # post a file
+file_content=$(cat ./samplefile.txt | base64)
 curl --location 'http://'$ROOTIP':8080/peersdb/command' \
     --header 'Content-Type: application/json' \
     --data '{
@@ -10,7 +14,6 @@ curl --location 'http://'$ROOTIP':8080/peersdb/command' \
         "argcnt": 1,
         "cmd": "post"
       },
-      "args": [
-        ""
-      ]
+      "args": [],
+      "file": "'"$file_content"'"
     }'
