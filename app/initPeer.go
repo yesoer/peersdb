a9a2494ef8fb5c04221deb4ce40b438170aca490
yesoer <luisenglaender@web.de>
2023-07-09 21:40:29 +0200

    refactor(ipfs): handle bootstrapping on peersdb level
    This way we only need to know the IP to issue a connection. That in turn is important for the evaluation setup because we can't predict IDs.

--- app/initPeer.go
+++ app/initPeer.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"peersdb/config"
@@ -154,5 +158,85 @@ func InitPeer(peersDB *PeersDB) error {
 	peersDB.Config = conf
 	peersDB.Benchmark = &Benchmark{}
 
+	// connect to a bootstrap peer
+	if *config.FlagBootstrap != "" {
+		fmt.Print("\nbootstrap : ", *config.FlagBootstrap, "\n")
+		IssueConnectCmd(peersDB, []string{*config.FlagBootstrap})
+	}
+
 	return nil
+}
+
+// connect to a peer given their IP, by sending an http request for the "CONNECT"
+// cmd, with own connection string
+func IssueConnectCmd(peersDB *PeersDB, peers []string) {
+	client := &http.Client{}
+
+	// for each given ip, send the connect request
+	for _, p := range peers {
+		// TODO : can we get the address from the coreapi ?
+		myIP := getOwnIP()
+		if p == "127.0.0.1" {
+			myIP = p
+		}
+		myAddr := "/ip4/" + myIP + "/tcp/" + *config.FlagIPFSPort + "/p2p/" + peersDB.Config.PeerID
+		fmt.Print("\n sending my address : ", myAddr, " to IP ", p, "\n")
+
+		// TODO : port may be different aswell
+		cmdPath := "http://" + p + ":8080/peersdb/command"
+
+		connectReq := Request{CONNECT, []string{myAddr}}
+		jsonData, err := json.Marshal(connectReq)
+		if err != nil {
+			fmt.Println("Error marshaling JSON:", err)
+			return
+		}
+
+		req, err := http.NewRequest("POST", cmdPath, bytes.NewBuffer(jsonData))
+		if err != nil {
+			fmt.Println("Error creating request:", err)
+			continue
+		}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Error sending request:", err)
+			continue
+		}
+		defer resp.Body.Close()
+	}
+}
+
+func getOwnIP() string {
+	// get the list of network interfaces
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Failed to get network interfaces:", err)
+		return ""
+	}
+
+	// iterate through each network interface
+	for _, iface := range interfaces {
+		// check if the interface is up and not a loopback
+		if iface.Flags&net.FlagUp != 0 && iface.Flags&net.FlagLoopback == 0 {
+			// get the addresses for the current interface
+			addrs, err := iface.Addrs()
+			if err != nil {
+				fmt.Println("Failed to get addresses for interface", iface.Name, ":", err)
+				continue
+			}
+
+			// iterate through each address
+			for _, addr := range addrs {
+				// check if the address is an IP address
+				ipNet, ok := addr.(*net.IPNet)
+				if ok && !ipNet.IP.IsLoopback() && ipNet.IP.To4() != nil {
+					// print the IP address
+					return ipNet.IP.String()
+				}
+			}
+		}
+	}
+
+	return ""
 }
