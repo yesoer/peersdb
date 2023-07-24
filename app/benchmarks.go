00edbbdb0b6ebe7720552416748e768003d80c6e
yesoer <luisenglaender@web.de>
2023-07-25 01:05:05 +0200

    feat(benchmark): custom marshalling
    I simply didn't want to implement this data preperation in shell but this way we can't access raw data.

--- app/benchmarks.go
+++ app/benchmarks.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"time"
 )
@@ -21,6 +22,56 @@ type Benchmark struct {
 	// the region this node is working from
 	Region string `json:"region"`
 }
+
+// custom json marshal
+// TODO : doesn't seem like an ideal solution
+func (b *Benchmark) MarshalJSON() ([]byte, error) {
+	bootstrapSec := b.Bootstrap.Seconds()
+	newContributionsSec := make([]float64, len(b.NewContributions))
+	for i, c := range b.NewContributions {
+		newContributionsSec[i] = c.Seconds()
+	}
+
+	// initialize variables to store the smallest, largest, and sum of elements
+	smallest := 0.0
+	largest := 0.0
+	if len(newContributionsSec) > 0 {
+		smallest = newContributionsSec[0]
+		largest = newContributionsSec[0]
+	}
+	sum := 0.0
+
+	// loop through the slice to calculate the smallest, largest, and sum of elements
+	for _, val := range newContributionsSec {
+		if val < smallest {
+			smallest = val
+		}
+		if val > largest {
+			largest = val
+		}
+		sum += val
+	}
+
+	average := 0.0
+	if len(newContributionsSec) > 0 {
+		average = sum / float64(len(newContributionsSec))
+	}
+
+	data := struct {
+		BootrapSec float64 `json:"bootstrap"`
+		AverageC   float64 `json:"averagec"`
+		MinC       float64 `json:"minc"`
+		MaxC       float64 `json:"maxc"`
+		Region     string  `json:"region"`
+	}{
+		BootrapSec: bootstrapSec,
+		AverageC:   average,
+		MinC:       smallest,
+		MaxC:       largest,
+		Region:     b.Region,
+	}
+
+	return json.Marshal(data)
 }
 
 // bootstrapping-time is the time it takes a new node to gather all existing data.
